14ece2e7a1a79d14cabd603792b7fde180e135d5
nakednous <nakednous@gmail.com>
2014-01-27 18:11:00 -0500

    updating header

--- src/remixlab/proscene/Scene.java
+++ src/remixlab/proscene/Scene.java
@@ -1,12 +1,12 @@
-/*******************************************************************************
- * ProScene (version 2.0.0-alpha.3)
- * Copyright (c) 2013 National university of Colombia, https://github.com/remixlab
+/**************************************************************************************
+ * ProScene (version 2.0.0)
+ * Copyright (c) 2010-2014 National University of Colombia, https://github.com/remixlab
  * @author Jean Pierre Charalambos, http://otrolado.info/
  * 
  * All rights reserved. Library that eases the creation of interactive scenes
  * in Processing, released under the terms of the GNU Public License v3.0
  * which is available at http://www.gnu.org/licenses/gpl.html
- ******************************************************************************/
+ *************************************************************************************/
 package remixlab.proscene;
 
 import processing.core.*;
