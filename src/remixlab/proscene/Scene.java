4e33bde798fb9110ae6c10f14fb4eaeb89a63925
Jean Pierre Charalambos <nakednous@gmail.com>
2016-10-29 17:29:23 -0500

    enter new beta

--- src/remixlab/proscene/Scene.java
+++ src/remixlab/proscene/Scene.java
@@ -103,9 +103,9 @@ public class Scene extends AbstractScene implements PConstants {
   // end: GWT-incompatible
   // */
 
-  public static final String prettyVersion = "3.0.0-beta.8";
+  public static final String prettyVersion = "3.0.0-beta.9";
 
-  public static final String version = "30";
+  public static final String version = "31";
 
   // P R O C E S S I N G A P P L E T A N D O B J E C T S
   protected PApplet parent;
