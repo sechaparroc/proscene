65ba5bc364734d9756bae1799099ef52a0bc0c6e
Sebastian Chaparro <sechaparroc@unal.edu.co>
2017-08-19 22:58:37 -0500

    Added Planar Polygon constraint

--- src/remixlab/dandelion/constraint/PlanarPolygon.java
+++ src/remixlab/dandelion/constraint/PlanarPolygon.java
@@ -22,8 +22,9 @@ public class PlanarPolygon extends Constraint{
     * */
 
     private ArrayList<Vec> vertices = new ArrayList<Vec>();
-    private float radius = 10.f;//Vertices must be inside a circle of radius r
+    private float height = 1.f;
     private Quat restRotation = new Quat();
+    private Vec min, max;
 
     public Quat getRestRotation() {
         return restRotation;
@@ -40,26 +41,39 @@ public class PlanarPolygon extends Constraint{
         this.vertices = vertices;
     }
 
-    public float getRadius() {
-        return radius;
+    public float getHeight() {
+        return height;
     }
 
-    public void setRadius(float radius) {
-        this.radius = radius;
+    public void setHeight(float height) {
+        this.height = height;
     }
 
     public PlanarPolygon(){
         vertices = new ArrayList<Vec>();
         restRotation = new Quat();
+        height = 5.f;
+    }
+
+    public PlanarPolygon(ArrayList<Vec> vertices, Quat restRotation, float height) {
+        this.vertices = vertices;
+        this.restRotation = restRotation.get();
+        this.height = height;
+        projectToPlane();
+        computeBoundingBox();
     }
 
     public PlanarPolygon(ArrayList<Vec> vertices, Quat restRotation) {
         this.vertices = vertices;
         this.restRotation = restRotation.get();
+        projectToPlane();
+        computeBoundingBox();
     }
 
     public PlanarPolygon(ArrayList<Vec> Vertices) {
         this.vertices = vertices;
+        projectToPlane();
+        computeBoundingBox();
     }
 
     @Override
@@ -85,12 +99,79 @@ public class PlanarPolygon extends Constraint{
     }
 
     public Vec getConstraint(Vec target, Quat restRotation){
-        Vec uvec    = Quat.multiply(restRotation,new Vec(0,1,0));
-        Vec rvec = Quat.multiply(restRotation,new Vec(1,0,0));
-        Vec line = Quat.multiply(restRotation, new Vec(0,0,1));
-
-        return null;
+        Vec uvec    = new Vec(0,1,0);
+        Vec rvec    = new Vec(1,0,0);
+        Vec line    = new Vec(0,0,1);
+        Vec point   = restRotation.inverse().multiply(target);
+        Vec proj    = new Vec(height*point.x()/point.z(),height*point.y()/point.z());
+        if(!isInside(proj)){
+            Vec constrained = closestPoint(proj);
+            constrained.setZ(height);
+            constrained.multiply(point.z()/height);
+            return restRotation.rotate(constrained);
+        }
+        return target;
     }
 
+    public void computeBoundingBox(){
+        for(Vec v : vertices){
+            if(v.x() < min.x()) min.setX(v.x());
+            if(v.y() < min.y()) min.setY(v.y());
+            if(v.x() > max.x()) max.setX(v.x());
+            if(v.y() > max.y()) max.setY(v.y());
+        }
+    }
 
+    public void projectToPlane(){
+        for(Vec v : vertices){
+            //Just not consider Z
+            v.setZ(0);
+        }
+    }
+
+    /*Code was transcript from https://wrf.ecse.rpi.edu//Research/Short_Notes/pnpoly.html*/
+    public boolean isInside(Vec point){
+        if(point.x() < min.x() || point.x() > max.x() ||
+                point.y() < min.y() || point.y() > max.y()) return false;
+        //Ray-casting algorithm
+        boolean c = false;
+        for(int i = 0, j = vertices.size()-1; i < vertices.size(); j = i++){
+            Vec v_i = vertices.get(i);
+            Vec v_j = vertices.get(j);
+            if ( ((v_i.y()>point.y()) != (v_j.y()>point.y())) &&
+                    (point.x() < (v_j.x()-v_i.x()) * (point.y()-v_i.y()) / (v_j.y()-v_i.y()) + v_i.x()));
+                        c = !c;
+        }
+        return c;
+    }
+
+    public Vec closestPoint(Vec point){
+        float minDist = 999999;
+        Vec target = new Vec();
+        for(int i = 0, j = vertices.size()-1; i < vertices.size(); j = i++){
+            Vec projection;
+            float dist;
+            Vec v_i = vertices.get(i);
+            Vec v_j = vertices.get(j);
+            Vec edge = Vec.subtract(v_i, v_j);
+            //Get distance to line
+            float t = Vec.dot(edge, Vec.subtract(point, v_j));
+            t /= Vec.subtract(point,v_j).magnitude();
+            if(t > 0){
+                dist = Vec.distance(v_j,point);
+                projection = v_j.get();
+            }else if(t > 1){
+                dist = Vec.distance(v_i,point);
+                projection = v_i.get();
+            }else{
+                projection = Vec.add(v_j, Vec.multiply(edge,t));
+                dist = Vec.subtract(point, projection).magnitude();
+            }
+            if(dist < minDist){
+                minDist = dist;
+                target = projection;
+            }
+        }
+        return target;
+    }
 }
