3909c4b17caa5345f8604a6933d27f242bfb472f
Sebastian Chaparro <sechaparroc@unal.edu.co>
2017-08-24 17:27:48 -0500

    IK tests updated

--- testing/src/ik/MultipleEndEffectorIK3Dv2.java
+++ testing/src/ik/MultipleEndEffectorIK3Dv2.java
@@ -1,9 +1,7 @@
 package ik;
 
 import processing.core.PApplet;
-import processing.core.PFont;
-import processing.core.PShape;
-import remixlab.dandelion.constraint.BallAndSocket;
+import processing.core.PGraphics;
 import remixlab.dandelion.core.Camera;
 import remixlab.dandelion.core.GenericFrame;
 import remixlab.dandelion.geom.Quat;
@@ -17,19 +15,22 @@ import java.util.ArrayList;
 
 /**
  * Created by sebchaparr on 27/06/17.
+ * A tree Structure with an IK Associated and a set of Targets (Red Spheres)
+ * Each Branch of a sub-base (A frame with 2 or more Children) does affect
+ * the orientation of the other Branches
  */
 public class MultipleEndEffectorIK3Dv2 extends PApplet{
-
-    Scene scene;
-    PFont myFont;
-    float distanceBtwnSliblings = 30.f;
+    //Frames Hierarchy parameters
     int numSliblings = 6;
     int numJoints = 8;
-
-    float constraint_factor = 50;
+    float distanceBtwnSliblings = 30.f;
     float boneLength = 20;
 
-    int TimesPerFrame = 40;
+    //Solver Parameters
+    int TimesPerFrame = 1;
+
+    //Scene Parameters
+    Scene scene;
     InteractiveFrame root;
     ArrayList<InteractiveFrame> targets = new ArrayList<InteractiveFrame>();//one target per Leaf
 
@@ -38,7 +39,72 @@ public class MultipleEndEffectorIK3Dv2 extends PApplet{
         size(500, 500, P3D);
     }
 
-    public ArrayList<InteractiveFrame> createBranch(GenericFrame root, int numSliblings, float boneLength){
+    public void setup() {
+        scene = new Scene(this);
+        scene.setCameraType(Camera.Type.ORTHOGRAPHIC);
+        scene.setAxesVisualHint(true);
+
+        for(int i = 0; i < numSliblings; i++)
+            targets.add(new InteractiveFrame(scene, "targetGraphics"));
+
+        root = new InteractiveFrame(scene);
+        ArrayList<InteractiveFrame> leaves;
+
+        leaves = generateTree(root,  numSliblings, boneLength);
+        int idx = leaves.size()/2;
+        leaves = generateTree(leaves.get(idx),  numSliblings, boneLength);
+        idx = leaves.size()/2;
+        leaves = generateTree(leaves.get(idx),  numSliblings, boneLength);
+        idx = leaves.size()/2;
+        leaves = generateTree(leaves.get(idx),  numSliblings, boneLength);
+
+        //Create IK Solver to the given Structure
+        Solver solver = scene.setIKStructure(root);
+        solver.setTIMESPERFRAME(TimesPerFrame);
+        solver.setMINCHANGE(0.001f);
+
+        //Create and associate Targets to each Leave at the last Level of the Structure
+        for(int i = 0; i < leaves.size(); i++){
+            targets.get(i).setPosition(leaves.get(i).position());
+            targets.get(i).setOrientation(leaves.get(i).orientation());
+            scene.addIKTarget(leaves.get(i), targets.get(i));
+        }
+    }
+
+    public void frameGraphics(InteractiveFrame iFrame, PGraphics pg) {
+        pg.pushStyle();
+        scene.drawAxes(pg, 3);
+        pg.fill(0, 255, 0);
+        pg.strokeWeight(5);
+        pg.stroke(0, 100, 100, 100);
+        if (iFrame.referenceFrame() != null) {
+            Vec v = iFrame.coordinatesOfFrom(new Vec(), iFrame.referenceFrame());
+            if(pg.is2D())
+                pg.line(0, 0, v.x(), v.y());
+            else
+                pg.line(0, 0, 0, v.x(), v.y(), v.z());
+
+        }
+        pg.popStyle();
+    }
+
+    public void targetGraphics(PGraphics pg) {
+        pg.pushStyle();
+        pg.noStroke();
+        pg.fill(255, 0, 0, 200);
+        if(pg.is2D())
+            pg.ellipse(0, 0, 5, 5);
+        else
+            pg.sphere(5);
+        pg.popStyle();
+    }
+
+    public void draw() {
+        background(0);
+        scene.drawFrames();
+    }
+
+    public ArrayList<InteractiveFrame> generateTree(GenericFrame root, int numSliblings, float boneLength){
         ArrayList<InteractiveFrame> frames = new ArrayList<InteractiveFrame>();
         //Generate children
         float step = PI/numSliblings;
@@ -47,73 +113,12 @@ public class MultipleEndEffectorIK3Dv2 extends PApplet{
             Vec vec = new Vec(0,boneLength, 0);
             Quat q = new Quat(new Vec(0,0,1), step*j - PI);
             vec = q.multiply(vec);
-            InteractiveFrame child = new InteractiveFrame(scene);
+            InteractiveFrame child = new InteractiveFrame(scene, "frameGraphics");
             child.setReferenceFrame(root);
             child.translate(vec);
             frames.add(child);
         }
         return frames;
-    }
-
-    public void setup() {
-        scene = new Scene(this);
-        scene.setCameraType(Camera.Type.ORTHOGRAPHIC);
-        scene.setAxesVisualHint(true);
-
-        for(int i = 0; i < numSliblings; i++)
-            targets.add(new InteractiveFrame(scene));
-
-        root = new InteractiveFrame(scene);
-        ArrayList<InteractiveFrame> leaves;
-        leaves = createBranch(root,  numSliblings, boneLength);
-        int idx = leaves.size()/2;
-        leaves = createBranch(leaves.get(idx),  numSliblings, boneLength);
-        idx = leaves.size()/2;
-        leaves = createBranch(leaves.get(idx),  numSliblings, boneLength);
-        idx = leaves.size()/2;
-        leaves = createBranch(leaves.get(idx),  numSliblings, boneLength);
-        //Fix hierarchy
-        //root.setupHierarchy();
-        Solver solver = scene.setIKStructure(root);
-
-        for(int i = 0; i < leaves.size(); i++){
-            targets.get(i).setPosition(leaves.get(i).position());
-            targets.get(i).setOrientation(leaves.get(i).orientation());
-            scene.addIKTarget(leaves.get(i), targets.get(i));
-        }
-        solver.setTIMESPERFRAME(TimesPerFrame);
-        solver.setMINCHANGE(0.001f);
-    }
-
-
-    public void draw() {
-        background(0);
-        for(InteractiveFrame j : scene.branch(root)){
-            pushMatrix();
-            pushStyle();
-            j.applyWorldTransformation();
-            scene.drawAxes(3);
-            fill(0,255,0);
-            strokeWeight(5);
-            stroke(0,100,100,100);
-            if(j.referenceFrame() != null){
-                Vec v = j.coordinatesOfFrom(new Vec(), j.referenceFrame());
-                line(0,0,0, v.x(), v.y(), v.z());
-            }
-            popStyle();
-            popMatrix();
-        }
-
-        for(GenericFrame target : targets) {
-            pushMatrix();
-            pushStyle();
-            noStroke();
-            fill(255, 0, 0, 200);
-            translate(target.position().x(), target.position().y(), target.position().z());
-            sphere(5);
-            popStyle();
-            popMatrix();
-        }
     }
 
     public static void main(String args[]) {
