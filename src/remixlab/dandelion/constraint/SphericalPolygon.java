960779f4e4d62c15fb6b030635c9668ca2f4530f
Sebastian Chaparro <sechaparroc@unal.edu.co>
2017-10-19 18:20:54 -0500

    Added Interactive Example

---
+++ src/remixlab/dandelion/constraint/SphericalPolygon.java
@@ -0,0 +1,7 @@
+package remixlab.dandelion.constraint;
+
+/**
+ * Created by sebchaparr on 30/08/17.
+ */
+public class SphericalPolygon {
+}
---
+++ testing/src/ik/InteractiveIK.java
@@ -0,0 +1,7 @@
+package ik;
+
+/**
+ * Created by sebchaparr on 19/10/17.
+ */
+public class InteractiveIK {
+}
