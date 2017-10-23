1865bb202e7a3843ecd683da678ece2df27b081c
Sebastian Chaparro <sechaparroc@unal.edu.co>
2017-10-22 21:13:14 -0500

    Interactive Example with Spherical Cosntraints added

--- testing/src/ik/InteractiveIKv2.java
+++ testing/src/ik/InteractiveIKv2.java
@@ -4,8 +4,11 @@ import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.core.PShape;
 import remixlab.bias.event.DOF1Event;
+import remixlab.bias.event.DOF2Event;
 import remixlab.bias.event.MotionEvent;
-import remixlab.dandelion.constraint.PlanarPolygon;
+import remixlab.dandelion.constraint.AxisPlaneConstraint;
+import remixlab.dandelion.constraint.LocalConstraint;
+import remixlab.dandelion.constraint.SphericalPolygon;
 import remixlab.dandelion.core.Camera;
 import remixlab.dandelion.core.GenericFrame;
 import remixlab.dandelion.geom.Frame;
@@ -20,9 +23,9 @@ import java.util.ArrayList;
 import static remixlab.proscene.MouseAgent.*;
 
 /**
- * Created by sebchaparr on 19/10/17.
+ * Created by sebchaparr on 20/10/17.
  */
-public class InteractiveIK extends PApplet {
+public class InteractiveIKv2 extends PApplet {
     Scene scene, auxScene;
     PGraphics canvas, auxCanvas;
     InteractiveShape interactiveShape;
@@ -58,36 +61,34 @@ public class InteractiveIK extends PApplet {
         target = new InteractiveFrame(scene, "targetGraphics");
         target.translate(0, 0,0 );
 
-        auxCanvas = createGraphics(oW, oH, P2D);
+        auxCanvas = createGraphics(oW, oH, P3D);
         auxScene = new Scene(this, auxCanvas, oX, oY);
         auxScene.setVisualHints(0);
         auxScene.setRadius(20);
         auxScene.showAll();
 
-        ArrayList<GenericFrame> branchPlanarConstraints = generateChain(num_joints, boneLength, new Vec(scene.radius(), -scene.radius(), 0));
+        ArrayList<GenericFrame> branchSphericalConstraints = generateChain(num_joints, boneLength, new Vec(scene.radius(), -scene.radius(), 0));
         //Sinus cone planar Polygon constraints
 
-        //Define the Base (Any Polygon in clockwise or Counterclockwise order)
-        ArrayList<Vec> vertices = new ArrayList<Vec>();
-        vertices.add(new Vec(-10,-10));
-        vertices.add(new Vec(10,-10));
-        vertices.add(new Vec(10, 10));
-        vertices.add(new Vec(-10,10));
-
-        for (int i = 1; i < branchPlanarConstraints.size()-1; i++) {
-            PlanarPolygon constraint = new PlanarPolygon(vertices);
-            constraint.setHeight(boneLength/2.f);
-            constraint.setRestRotation((Quat) branchPlanarConstraints.get(i).rotation().get());
-            branchPlanarConstraints.get(i).setConstraint(constraint);
-            //assign 1 Click Event
-            ((InteractiveFrame)branchPlanarConstraints.get(i)).setClickBinding(LEFT, 1, "setCurrentFrame");
-
+        //Define the Base (Any Polygon in Counterclockwise order)
+        ArrayList<Vec> verticesSpherical = new ArrayList<Vec>();
+        int numVertices = 12;
+        for(int i = 0; i < numVertices; i++){
+            float step = i*(2*PI/(float)numVertices);
+            verticesSpherical.add(new Vec(cos(step),sin(step), random(0,1)));
         }
 
-        Solver solverPlanarConstraints = scene.setIKStructure(branchPlanarConstraints.get(0));
-        scene.addIKTarget(branchPlanarConstraints.get(branchPlanarConstraints.size()-1), target);
-        solverPlanarConstraints.setTIMESPERFRAME(TimesPerFrame);
+        for (int i = 1; i < branchSphericalConstraints.size()-1; i++) {
+            SphericalPolygon constraint = new SphericalPolygon(verticesSpherical);
+            constraint.setRestRotation((Quat) branchSphericalConstraints.get(i).rotation().get());
+            branchSphericalConstraints.get(i).setConstraint(constraint);
+            //assign 1 Click Event
+            ((InteractiveFrame)branchSphericalConstraints.get(i)).setClickBinding(LEFT, 1, "setCurrentFrame");
+        }
 
+        Solver solverSphericalConstraints = scene.setIKStructure(branchSphericalConstraints.get(0));
+        scene.addIKTarget(branchSphericalConstraints.get(branchSphericalConstraints.size()-1), target);
+        solverSphericalConstraints.setTIMESPERFRAME(TimesPerFrame);
 
         interactiveShape = new InteractiveShape(auxScene);
     }
@@ -104,11 +105,11 @@ public class InteractiveIK extends PApplet {
                 canvas.pushMatrix();
                 canvas.pushStyle();
                 Frame reference = new Frame(frame.position(), Quat.compose(frame.orientation(), frame.rotation().inverse()));
-                if(frame.constraint() instanceof PlanarPolygon){
+                if(frame.constraint() instanceof SphericalPolygon){
                     boolean current = interactiveShape.getIFrame() == frame;
-                    reference.rotate(((PlanarPolygon)frame.constraint()).getRestRotation());
+                    reference.rotate(((SphericalPolygon)frame.constraint()).getRestRotation());
                     scene.applyWorldTransformation(reference);
-                    drawCone(canvas,((PlanarPolygon)frame.constraint()).getHeight(),((PlanarPolygon)frame.constraint()).getVertices(), current);
+                    drawCone(canvas,((SphericalPolygon)frame.constraint()).getVertices(),boneLength,current);
                 }
                 canvas.popStyle();
                 canvas.popMatrix();
@@ -150,9 +151,9 @@ public class InteractiveIK extends PApplet {
 
     public void setCurrentFrame(InteractiveFrame frame){
         if(frame.constraint() != null) {
-            if (frame.constraint() instanceof PlanarPolygon) {
+            if (frame.constraint() instanceof SphericalPolygon) {
                 interactiveShape.setIFrame(frame);
-                interactiveShape.setVertices(((PlanarPolygon)frame.constraint()).getVertices());
+                interactiveShape.setVertices(((SphericalPolygon)frame.constraint()).getVertices());
             }
         }else{
             interactiveShape.setIFrame(null);
@@ -187,30 +188,30 @@ public class InteractiveIK extends PApplet {
         pg.popStyle();
     }
 
-    public void drawCone(PGraphics pg, float height, ArrayList<Vec> vertices, boolean current){
+    public void drawCone(PGraphics pg, ArrayList<Vec> vertices, float scale, boolean current){
         pg.pushStyle();
         pg.noStroke();
         if(current) pg.fill(255,0,0,80);
         else pg.fill(246,117,19,80);
+        fill(246,117,19,80);
         pg.beginShape(PApplet.TRIANGLE_FAN);
         pg.vertex(0, 0, 0);
         for (Vec v : vertices) {
-            pg.vertex( v.x(), v.y(), height);
+            pg.vertex( scale*v.x(), scale*v.y(), scale*v.z());
         }
-        if(!vertices.isEmpty()) pg.vertex( vertices.get(0).x(), vertices.get(0).y(), height);
+        if(!vertices.isEmpty()) pg.vertex( scale*vertices.get(0).x(), scale*vertices.get(0).y(), scale*vertices.get(0).z());
         pg.endShape();
         pg.popStyle();
     }
 
-
     public static void main(String args[]) {
-        PApplet.main(new String[]{"ik.InteractiveIK"});
+        PApplet.main(new String[]{"ik.InteractiveIKv2"});
     }
 
     //Required Auxiliar Classes
     public static class InteractivePoint extends InteractiveFrame{
         InteractiveShape interactiveShape;
-        float radius = 2.f;
+        float radius = 0.8f;
 
 
         public InteractivePoint(Scene scn) {
@@ -225,9 +226,9 @@ public class InteractiveIK extends PApplet {
             super(scn, (GenericFrame) interactiveShape);
             this.interactiveShape = interactiveShape;
             this.setMotionBinding(RIGHT_ID, "customTranslate");
-            PShape shape = scene().pApplet().createShape(ELLIPSE, 0,0, radius, radius);
+            PShape shape = scene().pApplet().createShape(SPHERE, radius);
             shape.setStroke(scene().pApplet().color(255,0,0));
-            shape.setFill(scene().pApplet().color(255,0,0));
+            shape.setFill(scene().pApplet().color(255,0,0,100));
             setShape(shape);
 
         }
@@ -241,15 +242,22 @@ public class InteractiveIK extends PApplet {
         }
 
         public void customTranslate(MotionEvent event){
-            this.translate(event);
-            //TODO : Instead of re build the shape, just modify the proper vertex
+            DOF2Event dof2Event = MotionEvent.dof2Event(event, true);
+            Vec vec = screenToVec(Vec.multiply(new Vec(isEyeFrame() ? -dof2Event.dx() : dof2Event.dx(),
+                    (gScene.isRightHanded() ^ isEyeFrame()) ? -dof2Event.dy() : dof2Event.dy(), 0.0f), this.translationSensitivity()));
+            //Translation is allowed only on sphere
+            vec.add(translation());
+            vec.normalize();
+            vec.multiply(interactiveShape.getRadius());
+            setTranslation(vec);
             interactiveShape.setUpdate(true);
         }
     }
 
     public static class InteractiveShape extends InteractiveFrame{
         private ArrayList<InteractivePoint> vertices = new ArrayList<InteractivePoint>();
-        private InteractiveFrame iFrame;
+        private InteractiveFrame iFrame; //Current
+
         private float radius = 10.f;
         private boolean update = false;
 
@@ -257,22 +265,50 @@ public class InteractiveIK extends PApplet {
         public InteractiveShape(Scene scn) {
             super(scn);
             setMotionBinding(WHEEL_ID, "changePolygon");
+            this.setHighlightingMode(HighlightingMode.NONE);
+            LocalConstraint constraint = new LocalConstraint();
+            constraint.setTranslationConstraint(AxisPlaneConstraint.Type.FORBIDDEN, new Vec());
+            this.setConstraint(constraint);
+            PShape shape = scene().pApplet().createShape(SPHERE, radius);
+            shape.setStroke(false);
+            shape.setFill(scene().pApplet().color(0,255,50));
+            this.setShape(shape);
             vertices = buildShape(10);
         }
 
         public InteractiveShape(Scene scn, GenericFrame referenceFrame) {
             super(scn, referenceFrame);
+            this.setHighlightingMode(HighlightingMode.NONE);
+            LocalConstraint constraint = new LocalConstraint();
+            constraint.setTranslationConstraint(AxisPlaneConstraint.Type.FORBIDDEN, new Vec());
+            this.setConstraint(constraint);
             vertices = buildShape(10);
         }
 
         public InteractiveShape(Scene scn, PShape ps) {
             super(scn, ps);
+            this.setHighlightingMode(HighlightingMode.NONE);
+            LocalConstraint constraint = new LocalConstraint();
+            constraint.setTranslationConstraint(AxisPlaneConstraint.Type.FORBIDDEN, new Vec());
+            this.setConstraint(constraint);
             vertices = buildShape(10);
         }
 
         public InteractiveShape(Scene scn, GenericFrame referenceFrame, PShape ps) {
             super(scn, referenceFrame, ps);
+            this.setHighlightingMode(HighlightingMode.NONE);
+            LocalConstraint constraint = new LocalConstraint();
+            constraint.setTranslationConstraint(AxisPlaneConstraint.Type.FORBIDDEN, new Vec());
+            this.setConstraint(constraint);
             vertices = buildShape(10);
+        }
+
+        public float getRadius() {
+            return radius;
+        }
+
+        public void setRadius(float radius) {
+            this.radius = radius;
         }
 
         public int getK(){
@@ -294,7 +330,10 @@ public class InteractiveIK extends PApplet {
             for(int i = 0; i < k; i++){
                 float theta = step*i;
                 InteractivePoint vertex = new InteractivePoint(scene(),this);
-                vertex.setTranslation(radius*cos(theta),radius*sin(theta));
+                Vec vec = new Vec(radius*cos(theta),radius*sin(theta),2*radius);
+                vec.normalize();
+                vec.multiply(radius);
+                vertex.setTranslation(vec);
                 vertices.add(vertex);
             }
             update = true;
@@ -311,15 +350,6 @@ public class InteractiveIK extends PApplet {
         //Must be called each time the Shape is Modified
         public void updateShape(){
             if(!update) return;
-            PShape shape = scene().pApplet().createShape();
-            shape.setStroke(false);
-            shape.setFill(scene().pApplet().color(0,255,0, 100));
-            shape.beginShape();
-            for(InteractiveFrame vertex : vertices){
-                shape.vertex(vertex.translation().x(), vertex.translation().y());
-            }
-            shape.endShape(CLOSE);
-            this.setShape(shape);
             updateIFrame();
             update = false;
         }
@@ -336,7 +366,6 @@ public class InteractiveIK extends PApplet {
             removeVertices();
             vertices = buildShape(getK() + times);
             updateShape();
-            updateIFrame();
         }
 
         public void removePoint(int times){
@@ -344,7 +373,6 @@ public class InteractiveIK extends PApplet {
             removeVertices();
             vertices = buildShape(getK() - times);
             updateShape();
-            updateIFrame();
         }
 
         public void changePolygon(DOF1Event dof1Event ){
@@ -356,13 +384,13 @@ public class InteractiveIK extends PApplet {
 
         public void updateIFrame(){
             if(iFrame != null)
-                ((PlanarPolygon) iFrame.constraint()).setVertices(getVertices());
+                ((SphericalPolygon) iFrame.constraint()).setVertices(getVertices());
         }
 
         public ArrayList<Vec> getVertices(){
             ArrayList<Vec> vertices = new ArrayList<Vec>();
             for(InteractivePoint point : this.vertices){
-                vertices.add(point.position().get());
+                vertices.add(point.translation().get());
             }
             return vertices;
         }
@@ -370,11 +398,11 @@ public class InteractiveIK extends PApplet {
         public void setVertices(ArrayList<Vec> vertices){
             if(vertices.size() < 3) return;
             removeVertices();
-            this.vertices.clear();
+            this.vertices = new ArrayList<InteractivePoint>();
             for(Vec vec : vertices){
-                InteractivePoint vertex = new InteractivePoint(this.scene(),this);
+                InteractivePoint vertex = new InteractivePoint(scene(),this);
+                vertex.setPosition(Vec.multiply(vec, radius));
                 scene().mouseAgent().addGrabber(vertex);
-                vertex.setPosition(vec);
                 this.vertices.add(vertex);
             }
             update = true;
